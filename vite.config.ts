01f67e86b02c486f832713e78eced46b1c1e8876
Mailar <83369389+devmailar@users.noreply.github.com>
2024-11-14 11:02:17 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		exclude: ["lucide-react"],
-	},
 });
